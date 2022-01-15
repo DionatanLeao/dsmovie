3375c3d2072e25be2eb7db99de8de3d6b25ac602
DionatanLeao <dionatan.ti@outlook.com>
2022-01-15 12:04:06 -0300

    Show score

--- frontend/src/components/MovieStars/index.tsx
+++ frontend/src/components/MovieStars/index.tsx
@@ -3,7 +3,32 @@ import { ReactComponent as StarHalf } from 'assets/img/star-half.svg';
 import { ReactComponent as StarEmpty } from 'assets/img/star-empty.svg';
 import './styles.css';
 
-function MovieStars() {
+type Props = {
+    score: number
+}
+
+function getFills(score: number) {
+
+    const fills = [0, 0, 0, 0, 0];
+
+    const integerPart = Math.floor(score);
+
+    for (let i = 0; i < integerPart; i++) {
+        fills[i] = 1;
+    }
+
+    const diff = score - integerPart;
+    if (diff > 0) {
+        fills[integerPart] = 0.5;
+    }
+
+    return fills;
+}
+
+function MovieStars({ score }: Props) {
+
+    const fills = getFills(score)
+
     return (
         <div className="dsmovie-stars-container">
             <StarFull />
